e706ea2a7a2390ed254646e2ae544df9bf78dae1
Sandro <sandromedeirosdacunha@gmail.com>
2023-07-05 10:22:58 -0300

    Entrega

--- js/script.js
+++ js/script.js
@@ -1,15 +1,49 @@
-function getForm(){
-    pessoa ={
-        nome : document.getElementById("nome").value,
-        peso : document.getElementById("peso").value,
-        altura : document.getElementById("altura").value,
-        genero : document.querySelector('input[name="sexo"]:checked').value
-    }
-    console.log(pessoa)
+function getForm() {
+    const form = document.getElementById('form-imc');
+    const nome = document.getElementById('nome').value;
+    const peso = parseFloat(document.getElementById('peso').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+    const sexo = document.querySelector('input[name="sexo"]:checked').value;
+    
+    let avaliacao = getAvaliacao(peso, altura, sexo);
+
+    addToTable(nome, avaliacao.imc, avaliacao.msg);
+
+    form.reset();
+
     return false;
 }
 
-function calcImc(peso, altura){
-    imc = peso / altura ** 2;
-    
+function getAvaliacao(peso, altura, sexo) {
+    let imc = peso / altura ** 2;
+    let msg;
+    if (sexo == "m" && imc < 19 || sexo == "f" && imc < 18) {
+        msg = 'Abaixo do peso';
+    } else if (sexo == "m" && imc < 25 || sexo == "f" && imc < 24) {
+        msg = 'Peso normal';
+    } else if (sexo == "m" && imc < 32 || sexo == "f" && imc < 29) {
+        msg = 'Sobrepeso';
+    } else {
+        msg = 'Obesidade';
+    }
+    return {imc: imc, msg: msg};
 }
+
+function addToTable(nome, imc, avaliacao) {
+    let tabela = document.getElementById('tabela-imc');
+    tabela.removeAttribute("hidden")
+    let newRow = tabela.insertRow();
+    let nomeCell = newRow.insertCell(0);
+    let imcCell = newRow.insertCell(1);
+    let avaliacaoCell = newRow.insertCell(2);
+    nomeCell.innerHTML = nome;
+    imcCell.innerHTML = imc.toFixed(2);
+    avaliacaoCell.innerHTML = avaliacao;
+}
+
+
+
+
+
+
+
